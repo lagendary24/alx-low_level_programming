7bc8be3c86d2b21010773c79d02c15d937c2e5c1
lagendary24 <acelagendary2424@gmail.com>
2022-09-08 18:38:34 -0700

    Task 4 headache

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - prints 0 if success
+ * Description: main - prints 0 if success
  * Return: 0 if success
  */
 int main(void)
 {
-	puts("\programming is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
