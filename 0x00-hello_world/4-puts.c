b54fcb68cf5fa6ce9923df4b6b0c03844cb01d27
lagendary24 <acelagendary2424@gmail.com>
2022-09-08 17:52:14 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#inlude <stdio.h>
-
+#include <stdio.h>
 /**
- *main - Printing
- *
- *Return: Always 0 (Successful)
+ * main - Printing
+ * 
+ * Return: Always 0 (Successful)
  */
 int main(void)
-{
-    puts("\"Programming is like building a multiple puzzle");
+{ 
+    puts("\"Programming is like building a multilingual
+puzzle");
     return (0);
 }
