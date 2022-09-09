ee880fb1f98c399e8bbbb4cf3c449c96f395c5c3
lagendary24 <acelagendary2424@gmail.com>
2022-09-08 19:02:29 -0700

    Task 5 i beg you

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Description: main - Prints a text studout
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+ 
