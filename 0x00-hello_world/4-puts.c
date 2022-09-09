bedfb1b66036071ddf01b139aa169bb06945804a
lagendary24 <acelagendary2424@gmail.com>
2022-09-08 18:12:22 -0700

    Task4 grace

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- * main - Printing
- * 
- * Return: Always 0 (Successful)
+ * description: main - Prints out a string to studout 
+ * Return: print out 0 if success
  */
 int main(void)
 { 
-    puts("\"Programming is like building a multilingual
-puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
