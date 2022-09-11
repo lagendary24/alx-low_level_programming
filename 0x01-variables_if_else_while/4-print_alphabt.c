5dc5b151c8fc21b05a88e493ed854e7a8e4ea04e
lagendary24 <acelagendary2424@gmail.com>
2022-09-11 16:38:26 -0700

    Task 4 done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <time>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 if Success
+ */
+int main(void)
+{
+	char low, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		if (low != e && low ! = q)
+		       putchar(low);
+	}
+printf("\n");
+return (0);
+}
