de896e5f64ca226f30e4929abbd455cb4729f74f
lagendary24 <acelagendary2424@gmail.com>
2022-09-11 21:07:42 -0700

    TASK 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,32 +2,29 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Entry point
+ * main - Prints single digit numbers starting from 0
  *
- * Return: Always 0 if (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n1 = 0, n2;
-	while(n1 <= 9)
-	{
-		n2 = 0;
-		while(n2 <= 9)
-		{
-			if(n1 != n2 && n1 < n2)
-			{
-				putchar(n1 + 48);
-				putchar(n2 + 48);
+	int i, j;
 
-				if(n1 + n2 != 17)
+	for(i = 0; i < = 9; i++)
+	{
+		for(j = 1; j <= 9; j++)
+		{
+			if(j > i)
+			{
+				putchar(i + '0');
+				putchar(i + '0');
+				if(i != 8)
 				{
 					putchar(',');
 					putchar(' ');
 				}
 			}
-			++n2
 		}
-		++n1
 	}
 	putchar('\n');
 	return (0);
