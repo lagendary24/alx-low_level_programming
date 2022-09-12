84882432b92331308fb8ac5fc2067788aa41e2d7
lagendary24 <acelagendary2424@gmail.com>
2022-09-12 15:08:00 -0700

    task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 if Success
+ */
+int main(void)
+{
+	int n1 = 0, n2, n3;
+
+	while (n1 <= 9)
+	{
+		n2 = 0;
+		while (n2 <=9)
+		{
+
+			n3 = 0;
+			while (n3 <= 9)
+			{
+				if (n1 != n2 && n1 < n2 && n2 != n3 && n2 < n3)
+				{
+					putchar(n1 + 48);
+					putchar(n2 + 48);
+					putchar(n3 + 48);
+
+					if (n1 + n2 + n3 != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				++n3;
+			}
+			++n2;
+		}
+		++n1;
+	}
+	putchar('\n');
+	return (0);
+}
