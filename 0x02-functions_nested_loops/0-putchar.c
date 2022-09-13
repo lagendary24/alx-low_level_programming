45d344ae775804a0db0205d794ceacc01d90a2ce
lagendary24 <acelagendary2424@gmail.com>
2022-09-13 14:22:11 -0700

    TASK 0 done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#inlude "main.h"
+
+/**
+ * main - Prints putchar
+ * Return: Always 0 if Success
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
