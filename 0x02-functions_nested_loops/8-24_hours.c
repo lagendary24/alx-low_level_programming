bf6a6cd8ef6df6b59ef04685f08b9f47b155d6dd
lagendary24 <acelagendary2424@gmail.com>
2022-09-13 20:06:22 -0700

    TASK 8 CLEANSED

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,7 +5,7 @@
  */
 void jack_bauer(void)
 {
-	init i, j;
+	int i, j;
 
 	for (i = 0; i < 24; i++)
 	{
@@ -14,7 +14,7 @@ void jack_bauer(void)
 			if (i < 10)
 			{
 				_putchar('0');
-				_putchar(i + '0')
+				_putchar(i + '0');
 			}
 			else if (i >= 10)
 			{
