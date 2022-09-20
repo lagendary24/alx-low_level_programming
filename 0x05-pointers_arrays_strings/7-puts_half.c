3ee6528fcdbd819ee3a3a24fcd6fa1672d3a0b43
lagendary24 <acelagendary2424@gmail.com>
2022-09-19 21:18:57 -0700

    tasks fixed

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,6 +15,7 @@ void puts_half(char *str)
 	{
 		j++;
 	}
+
 	if (j % 2 == 1)
 	{k = (j - 1) / 2;
 		k += 1;
@@ -23,6 +24,7 @@ void puts_half(char *str)
 	{
 		k = j / 2;
 	}
+
 	for (; k < j; k++)
 	{
 		_putchar(str[k]);
