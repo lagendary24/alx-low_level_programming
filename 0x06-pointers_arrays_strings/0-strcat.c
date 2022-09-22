8e1b1d462ff3309841574e1b2f1827e731f7203f
lagendary24 <acelagendary2424@gmail.com>
2022-09-21 21:55:06 -0700

    TASK

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,19 +10,19 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int dlen = 0, i;
+	int len = 0, i;
 
-	while (dest[dlen])
+	while (dest[len])
 	{
-		dlen++;
+		len++;
 	}
 
 	for (i = 0; src[i] != i++)
 	{
-		dest[dlen] = src[i];
-		dlen += 1;
+		dest[len] = src[i];
+		len += 1;
 	}
 
-	dest[dlen] = '\0';
+	dest[len] = '\0';
 	return (dest);
 }
