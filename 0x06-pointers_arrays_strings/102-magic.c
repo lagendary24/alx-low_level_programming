1bbaf14c4496b1cfc8791147598dd99edc0c661e
lagendary24 <acelagendary2424@gmail.com>
2022-09-23 14:26:30 -0700

    TASKS DONE

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include 
 
 int main(void)
 {
@@ -9,12 +9,12 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 	/*
-	 * Write your line of code here...
+	 * write your line of code here...
 	 * Remember:
-	 * - You are not allowed to use a
-	 * - You are not allowed to modify p
-	 * - Only one statement
-	 * - You are not allowed to code anything else than this line of code
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
 	 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
