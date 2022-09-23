d54da151f2c76bee3c7cb16fbe7131b7ba8a471c
lagendary24 <acelagendary2424@gmail.com>
2022-09-23 15:39:01 -0700

    tasks

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,17 +9,17 @@
 char *leet(char *str)
 {
 	int index1 = 0, index2;
-	char leet[8] = {'0', '1', '?', '3', '4', '?', '?', '7'};
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
-	while (str[++index1])
+	while (str[index1])
 	{
 		for (index2 = 0; index2 <= 7; index2++)
 		{
 			if (str[index1] == leet[index2] ||
 			    str[index1] - 32 == leet[index2])
-				str[index1] = index2 + '0';
+				str[index1] = index2;
+			index2++
 		}
-
 		index1++;
 	}
 
