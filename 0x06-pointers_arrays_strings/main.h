08810521bac2484f7533dd24e1fbb7a2909387c7
lagendary24 <acelagendary2424@gmail.com>
2022-09-23 16:52:38 -0700

    tasks

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,3 +11,5 @@ char *rot13(char *);
 void print_number(int n);
 int main(void);
 int main(void);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
