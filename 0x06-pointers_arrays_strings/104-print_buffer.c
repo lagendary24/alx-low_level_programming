c2c2072fdd3bceed2c8f5840e2bb2c9d073109fa
lagendary24 <acelagendary2424@gmail.com>
2022-09-25 14:55:50 -0700

    task

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -22,7 +22,7 @@ void print_buffer(char *b, int size)
 
 	for (byte = 0; byte < size; byte += 10)
 	{
-		printf("08x: ", byte);
+		printf("%08x: ", byte);
 
 		for (index = 0; index < 10; index++)
 		{
@@ -30,7 +30,7 @@ void print_buffer(char *b, int size)
 				printf(" ");
 
 			else
-				printf("%02", *(b + index + byte));
+				printf("%02x", *(b + index + byte));
 
 			if ((index % 2) != 0 && index != 0)
 				printf(" ");
