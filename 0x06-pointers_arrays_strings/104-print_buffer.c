471d72158bcea99524dd61e10cc028824543d625
lagendary24 <acelagendary2424@gmail.com>
2022-09-25 05:52:51 -0700

    tasks' git commit -m tasks git push vim 104-print_buffer.c git commit -m tasks git commit -m "tasks" git push vim 104-print_buffer.c
    git commit -m "tasks

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -5,43 +5,40 @@
  * print_buffer - Prints a buffer
  * @b: The buffer to be printed.
  * @size: The number of bytes to be printed the buffer.
- * 
- * Return: void
  */
 void print_buffer(char *b, int size)
 {
-	int byte, index;
+	int i = 0,  j;
 
-	for (byte = 0; byte < size; byte += 10)
+	if (size < 0)
 	{
-		printf("%08x: ", byte);
+		printf('\n');
+		return;
+	}
 
-		for (index = 0; index < 10; index++)
+	while (i < size)
+	{
+		if (i % 10 == 0)
+			printf("%08x: ", i);
+		for (j = i; j < i + 9; j += 2)
 		{
-			if ((index + byte) >= size)
-				printf(" ");
-
+			if ((j < size) && ((j + 1) < size))
+				printf("%02x%02x: ", b[j], b[j + 1]);
 			else
-
-				printf("%02x", *(b + index + byte));
-			if ((index % 2) != 0 && index != 0)
+			{
+				while (++j <= i + 10)
+					printf(" ");
 				printf(" ");
+			}
 		}
-
-		for (index = 0; index < 10; index++)
+		for (j = i; j < i + 9 && j < size; j++)
 		{
-			if ((index + byte) >= size)
-				break;
-
-			else if (*(b + index + bytes) >= 31 &&
-					*(b + index + byte) <= 126)
-				printf("%c", *(b + index + byte))
-
+			if (b[j] >= 32 && b[j] <= 126)
+				printf("%c", b[j]);
 			else
 				printf(".");
 		}
-
-		if (byte >= size)
-			continue;
-
-		printf("
+		printf('\n');
+		i += 10;
+	}
+}
