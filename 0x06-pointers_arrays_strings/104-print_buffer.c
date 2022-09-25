b211631975a5527808c4c44f61c48081f042da47
lagendary24 <acelagendary2424@gmail.com>
2022-09-25 06:35:11 -0700

    task

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -23,7 +23,7 @@ void print_buffer(char *b, int size)
 
 	if (size < 0)
 	{
-		int printf('\n');
+		printf("\n");
 		return;
 	}
 
@@ -49,7 +49,7 @@ void print_buffer(char *b, int size)
 			else
 				printf(".");
 		}
-		int printf('\n');
+		printf("\n");
 		i += 10;
 	}
 }
