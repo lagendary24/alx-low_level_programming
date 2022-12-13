ba5a6af27cb12f55db45aca36a116112f9916456
lagendary24 <acelagendary2424@gmail.com>
2022-12-13 05:56:49 -0800

    Done

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper -> this is a function to check upper case alphas
+ * @c: an integer paramer
+ * Return: something but Success
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+
+}
---
+++ 0x18-dynamic_libraries/funcs/README.md
@@ -0,0 +1,1 @@
+C - Funcs - Dynamic Libraries @README.md
